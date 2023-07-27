4f64849043a4b149e0a08454f69e0d89e87ee579
MD Ikramul Kayes <67923321+ikramulkayes@users.noreply.github.com>
2023-07-27 22:12:37 +0600

    Create datatypes.sol

---
+++ datatypes.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract SimpleStorage{
+    //Basic
+    bool dohave = true;
+    uint256 favnum = 88;
+    string favoriteNum = "yo pops";
+    int256 favint = -99;
+    address myAddress = 0x5C997d8073CA92630aBB0E3BE7eC72188c92843f;
+    bytes32 mybyte = "5353jkl232";
+}
